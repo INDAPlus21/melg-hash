ff0af3223f9db69a6e0eb3162431f791337d52eb
Marcus Elg <melg@kth.se>
2022-02-02 12:09:54 +0100

    Support multiple tables + clean up

--- customdatabase.go
+++ customdatabase.go
@@ -6,15 +6,23 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
-var database map[string]string = make(map[string]string)
+var database = make(map[string]map[string]string)
+var default_database = "default"
 
 func main() {
 	fmt.Println("Loading database...")
 	load()
+
+	// Add default table
+	if len(database) == 0 {
+		database[default_database] = make(map[string]string)
+	}
+
 	get_input()
 }
 
@@ -32,42 +40,20 @@ func get_input() {
 			fmt.Println("Invalid command")
 		} else {
 			// Different commands
-			if values[0] == "ADD" {
-				if len(values) != 3 {
-					fmt.Println("Invalid input, format is: ADD <KEY> <VALUE>")
-				} else {
-					database[values[1]] = values[2]
-					fmt.Println("Sucessfully added entry with key", values[1], "and value", values[2])
-				}
-			} else if values[0] == "REMOVE" {
-				if len(values) != 2 {
-					fmt.Println("Invalid input, format is: REMOVE <KEY>")
-				} else {
-					_, ok := database[values[1]]
-					delete(database, values[1])
-					if ok {
-						fmt.Println("Sucessfully removed entry with key", values[1])
-					} else {
-						fmt.Println("Did not find entry with key", values[1])
-					}
-				}
-			} else if values[0] == "GET" {
-				if len(values) != 2 {
-					fmt.Println("Invalid input, format is: GET <KEY>")
-				} else {
-					value, ok := database[values[1]]
-					if ok {
-						fmt.Println("Key", values[1], "was found in database with value", value)
-					} else {
-						fmt.Println("Key", values[1], "was not found in database")
-					}
-				}
-			} else if values[0] == "LIST" {
-				fmt.Println("Database key and values:")
-				for key, value := range database {
-					fmt.Println(key + ":" + value)
-				}
-			} else {
+			switch values[0] {
+			case "ADD":
+				command_add(values)
+			case "REMOVE":
+				command_remove(values)
+			case "GET":
+				command_get(values)
+			case "LIST":
+				command_list(values)
+			case "ADDTABLE":
+				command_add_table(values)
+			case "LISTTABLES":
+				command_list_tables()
+			default:
 				fmt.Println("Invalid command")
 			}
 		}
@@ -76,50 +62,182 @@ func get_input() {
 	}
 }
 
-func load() {
-	// Check file
-	directory, _ := os.Getwd()
-	file_location := directory + "\\data.data"
-	file, err := os.Open(file_location)
+func command_add(values []string) {
+	if len(values) < 3 || len(values) > 4 {
+		fmt.Println("Invalid input, format is: ADD <KEY> <VALUE> [TABLE]")
+	} else {
+		name := default_database
+		if len(values) == 4 {
+			name = values[3]
+			_, ok := database[name]
+			if !ok {
+				fmt.Println("Invalid table name, there is no table with name", name)
+				return
+			}
+		}
 
-	if os.IsNotExist(err) {
-		fmt.Println("No file found to load data from")
-		return
-	} else if err != nil {
+		database[name][values[1]] = values[2]
+		fmt.Println("Sucessfully added entry with key", values[1], "and value", values[2])
+	}
+}
+
+func command_remove(values []string) {
+	if len(values) < 2 || len(values) > 3 {
+		fmt.Println("Invalid input, format is: REMOVE <KEY> [TABLE]")
+	} else {
+		name := default_database
+		if len(values) == 3 {
+			name = values[2]
+			_, ok := database[name]
+			if !ok {
+				fmt.Println("Invalid table name, there is no table with name", name)
+				return
+			}
+		}
+
+		_, ok := database[name][values[1]]
+		delete(database, values[1])
+		if ok {
+			fmt.Println("Sucessfully removed entry with key", values[1], "from database", name)
+		} else {
+			fmt.Println("Did not find entry with key", values[1], "in database", name)
+		}
+	}
+}
+
+func command_get(values []string) {
+	if len(values) < 2 || len(values) > 3 {
+		fmt.Println("Invalid input, format is: GET <KEY> [TABLE]")
+	} else {
+		name := default_database
+		if len(values) == 3 {
+			name = values[2]
+			_, ok := database[name]
+			if !ok {
+				fmt.Println("Invalid table name, there is no table with name", name)
+				return
+			}
+		}
+
+		value, ok := database[name][values[1]]
+		if ok {
+			fmt.Println("Key", values[1], "was found in database", name, "with value", value)
+		} else {
+			fmt.Println("Key", values[1], "was not found in database", name)
+		}
+	}
+}
+
+func command_list(values []string) {
+	if len(values) > 2 {
+		fmt.Println("Invalid input, format is: LIST [TABLE]")
+	} else {
+		name := default_database
+		if len(values) == 2 {
+			name = values[1]
+			_, ok := database[name]
+			if !ok {
+				fmt.Println("Invalid table name, there is no table with name", name)
+				return
+			}
+		}
+
+		fmt.Println("Database key and values:")
+		for key, value := range database[name] {
+			fmt.Println(key + ":" + value)
+		}
+	}
+}
+
+func command_add_table(values []string) {
+	if len(values) != 2 {
+		fmt.Println("Invalid input, format is: ADDTABLE <NAME>")
+	} else {
+		_, ok := database[values[1]]
+
+		if ok {
+			fmt.Println("Database with name", values[1], "already exists")
+		} else {
+			database[values[1]] = make(map[string]string)
+			fmt.Println("Successfully added database with name", values[1])
+		}
+	}
+}
+
+func command_list_tables() {
+	fmt.Println("List of tables:")
+	for key := range database {
+		fmt.Println(key)
+	}
+}
+
+func load() {
+	// Get all files
+	directory, _ := os.Getwd()
+	files, err := ioutil.ReadDir(directory)
+
+	if err != nil {
 		panic(err)
 	}
 
-	// Load data line for line
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		values := strings.Split(scanner.Text(), ":")
-		if len(values) != 2 {
-			fmt.Println("Line is not formatted correctly:", values)
+	for _, file := range files {
+		// Only .data files
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".data") {
 			continue
 		}
-		database[values[0]] = values[1]
-	}
 
-	file.Close()
+		file_name := file.Name()[:len(file.Name())-len(".data")]
+
+		// Check file
+		file_location := directory + "\\" + file.Name()
+		file, err := os.Open(file_location)
+
+		if os.IsNotExist(err) {
+			fmt.Println("No file found to load data from")
+			return
+		} else if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Loading database vales from file", file_location)
+
+		// Load data line for line
+		scanner := bufio.NewScanner(file)
+		database[file_name] = make(map[string]string)
+
+		for scanner.Scan() {
+			values := strings.Split(scanner.Text(), ":")
+			if len(values) != 2 {
+				fmt.Println("Line is not formatted correctly:", values)
+				continue
+			}
+
+			database[file_name][values[0]] = values[1]
+		}
+
+		file.Close()
+	}
 }
 
 func save() {
-	// Create (override) file
-	directory, _ := os.Getwd()
-	file_location := directory + "\\data.data"
-	file, err := os.Create(file_location)
+	// Create files for all tables
+	for table_name, table_values := range database {
+		// Create (override) file
+		directory, _ := os.Getwd()
+		file_location := directory + "\\" + table_name + ".data"
+		file, err := os.Create(file_location)
 
-	if err != nil {
-		fmt.Println("Error when creating file")
+		if err != nil {
+			fmt.Println("Error when creating file", file_location)
+		}
+
+		// Write all database data
+		writer := bufio.NewWriter(file)
+		for key, value := range table_values {
+			writer.WriteString(key + ":" + value + "\n")
+		}
+
+		writer.Flush()
+		file.Close()
 	}
-
-	// Write all database data
-	writer := bufio.NewWriter(file)
-	for key, value := range database {
-		writer.WriteString(key + ":" + value + "\n")
-	}
-
-	writer.Flush()
-	file.Close()
 }
