f8320e7b72ebfbd1766b718333037f4a1e79b2eb
Marcus Elg <melg@kth.se>
2022-02-05 16:08:47 +0100

    Support resizing of hashtable

--- hashtable.go
+++ hashtable.go
@@ -5,8 +5,9 @@ import (
 )
 
 type HashTable struct {
-	count  int16
-	values []list.List // Store tuples of key and value
+	count    int16
+	capacity int16
+	values   []list.List // Store tuples of key and value
 }
 
 type KeyValuePair struct {
@@ -22,12 +23,17 @@ func get_hash(hash_table *HashTable, input string) int16 {
 		hash = ((hash << 5) + hash) + int64(char)
 	}
 
-	return int16(hash % int64(len(hash_table.values)))
+	return int16(hash % int64(hash_table.capacity))
 }
 
 func create_table() *HashTable {
+	return create_table_capacity(10) // Default size
+}
+
+func create_table_capacity(capacity int16) *HashTable {
 	hash_table := HashTable{}
-	hash_table.values = make([]list.List, 10) // Default size
+	hash_table.values = make([]list.List, capacity)
+	hash_table.capacity = capacity
 	return &hash_table
 }
 
@@ -43,9 +49,15 @@ func insert_value_table(hash_table *HashTable, key string, value string) {
 	hash_table.values[hash].PushFront(KeyValuePair{key, value})
 	hash_table.count += 1
 
-	// Resize hash table
-	if float32(hash_table.count) > 0.7*float32(hash_table.count) {
+	// Resize hash table (double the size)
+	if float32(hash_table.count) > 0.7*float32(hash_table.capacity) {
+		old_values := get_all_table(hash_table)
+		*hash_table = *create_table_capacity(hash_table.capacity * 2)
 
+		// Insert all old values
+		for _, key_value := range old_values {
+			insert_value_table(hash_table, key_value.key, key_value.value)
+		}
 	}
 }
 
